c7c2de617ea6c34ae52b471c75279524f5d0749c
JacobShanklin <Jacob.Shanklin.2@gmail.com>
2019-07-15 09:50:23 -0400

    appointment updates

--- PatientOnboarding-PatientService/src/main/java/com/patientonboarding/controller/AppointmentController.java
+++ PatientOnboarding-PatientService/src/main/java/com/patientonboarding/controller/AppointmentController.java
@@ -1,5 +1,7 @@
 package com.patientonboarding.controller;
 
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.data.annotation.Persistent;
@@ -32,11 +34,25 @@ public class AppointmentController {
 	@GetMapping(value="/getAppointment")
 	public Appointment getPatient(@RequestParam String dateParam, @RequestParam String timeParam) {
 		
-		appointment = new Appointment("7/9/2019","11:00 am");
+		// String dateRequested = getDateFromString(dateParam);
+		appointment = new Appointment(dateParam,timeParam);
+		appointmentDao.save(appointment);
 		
 		return appointment;	
 	}
-	
+/*
+	private String getDateFromString(String dateParam) {
+		Calendar date1 = Calendar.getInstance();
+		date1.set(2019, 06, 12);
+
+		while (date1.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+		    date1.add(Calendar.DATE, 1);
+		}
+
+		System.out.println(date1.getTime());
+		return null;
+	}
+*/	
 	
 
 }
